41f1d11c4c3429796df6301a7deb9040aacfe6d5
yujaewuk <wodnr3442@naver.com>
2024-08-09 12:13:56 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+// let firstmenu = document.querySelector(".menu > ul > li");
+// let header = document.querySelector(".header_area");
+
+// firstmenu.addEventListener("mouseenter", () => {
+//   header.style.transition = "height 0.3s";
+//   header.style.height = "300px";
+// });
+
+// firstmenu.addEventListener("mouseleave", () => {
+//   header.style.transition = "height 0.3s";
+//   header.style.height = "50px";
+// });
